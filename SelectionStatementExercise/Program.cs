d7b5ea063cfdaf4163d22caabbd1016e3f7204ec
Miles Mao <mrmao1@gmail.com>
2022-12-30 11:29:06 -0600

    selection exercise 1 results

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -3,8 +3,27 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {var r = new Random();
+            //var favNumber = r.Next(1, 9);
+            var favNumber = 6;
+
+            Console.WriteLine("Try to guess my favorite number");
+                var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine($"{userInput} is too low");
+
+            }
+            else if (userInput > favNumber) 
+            { 
+                Console.WriteLine($"{userInput} is too high");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
         }
     }
 }
